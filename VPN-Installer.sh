bad5c8174525336d75be03c80084c9d6a126a53d
realizer-vault <ramaramadhanp@gmail.com>
2025-12-01 23:59:20 +0700

    VPN-Installer.sh

--- VPN-Installer.sh
+++ VPN-Installer.sh
@@ -1,30 +1,36 @@
 #!/bin/bash
-# openvpn-install.sh file
+# ==Download installation scripts from GitHub [Credit to Angristan]==
 curl -O https://raw.githubusercontent.com/realizer-vault/OVPN-WG-Installer/master/Etc/openvpn-install.sh
-chmod +x openvpn-install.sh
-# wireguard-install.sh file
 curl -O https://raw.githubusercontent.com/realizer-vault/OVPN-WG-Installer/master/Etc/wireguard-install.sh
+
+# ==Make scripts executable==
 chmod +x wireguard-install.sh
-# Create required folders in /root
+chmod +x openvpn-install.sh
+
+# ==Create required directories in /root==
 mkdir -p /root/ovpn.sh-for_menu
 mkdir -p /root/wg.sh-for_menu
 mkdir -p /root/move.sh-for_move_Client_file
 mkdir -p /root/OVPN_Client
 mkdir -p /root/WG_Client
-# Copy install scripts to /usr/local/bin
+
+# ==Copy scripts to /usr/local/bin /usr/==
 cp openvpn-install.sh /usr/local/bin/
 cp wireguard-install.sh /usr/local/bin/
-# Rename after copying
+
+# ==Rename scripts after copying==
 mv /usr/local/bin/openvpn-install.sh /usr/local/bin/ovpn.sh
 mv /usr/local/bin/wireguard-install.sh /usr/local/bin/wg.sh
-# Create move.sh in /usr/local/bin
+
+# ==Create move.sh to organize client config files in /usr/local/bin==
 cat << 'EOF' > /usr/local/bin/move.sh
 #!/bin/bash
 shopt -s nullglob   # avoid "nameless file"
 mv /root/*.ovpn /root/OVPN_Client/ 2>/dev/null
 mv /root/*.conf /root/WG_Client/ 2>/dev/null
 EOF
-# Make all 3 executable
+
+# ==Make all scripts executable==
 chmod +x /usr/local/bin/ovpn.sh
 chmod +x /usr/local/bin/wg.sh
 chmod +x /usr/local/bin/move.sh
